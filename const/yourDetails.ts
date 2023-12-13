c9e8d119d6752a707a790f67edb8bfef6d725300
sol-web <sesi3home@gmail.com>
2023-12-13 09:46:42 +0900

    commit

---
+++ const/yourDetails.ts
@@ -0,0 +1,6 @@
+// Replace this with your contract address
+export const myEditionDropContractAddress: string =
+  '0x95533Eba08606FFefA786E343b9fd0Cf5FE7B559'
+
+// Replace this with your token id
+export const tokenId: string = '1' //사용할 수 있는 토큰 두번쨰 것
